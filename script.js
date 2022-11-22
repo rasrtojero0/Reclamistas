803cb72cf45dd458711027d26842dac788c7761f
rasrtojero0 <98290587+rasrtojero0@users.noreply.github.com>
2022-11-22 00:35:57 -0300

    Codigos de rechazo con buscador

--- script.js
+++ script.js
@@ -32,3 +32,23 @@ function evaluar(){
     }
 }
 
+
+// ---------------------BUSCADOR---------------------------------
+
+document.addEventListener("keyup", e=>{
+
+    if (e.target.matches("#buscador")){
+  
+        if (e.key ==="Escape")e.target.value = ""
+  
+        document.querySelectorAll(".articulo").forEach(fruta =>{
+  
+            fruta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+              ?fruta.classList.remove("filtro")
+              :fruta.classList.add("filtro")
+        })
+  
+    }
+  
+  
+  })
